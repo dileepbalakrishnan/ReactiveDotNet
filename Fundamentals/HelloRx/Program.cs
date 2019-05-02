c169da094cd0d5adc6dd36619c13b428d1b99b88
Dileep Balakrishnan <dileepkrishnan@hotmail.com>
2019-05-03 00:06:19 +0530

    A simple program demonstrating RX in action.

---
+++ Fundamentals/HelloRx/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace HelloRx
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var query = "Hello, Rx.Net!".Select(c => c.ToString() + " ").ToObservable();
+            query.Subscribe(Console.Write, OnCompleted);
+        }
+
+        private static void OnCompleted()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Done");
+        }
+    }
+}
