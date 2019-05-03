3f31e91b61c0d9084e083d8fd37f4045ab7bce31
Dileep Balakrishnan <dileepkrishnan@hotmail.com>
2019-05-03 20:59:20 +0530

    Simple concurrency demo.

---
+++ Fundamentals/SimpleConcurrency/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using System.Threading;
+
+namespace SimpleConcurrency
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine($"Main Thread - {Thread.CurrentThread.ManagedThreadId}");
+            var query = "Hello, Rx.Net!".Select(c => c.ToString() + " ").ToObservable(NewThreadScheduler.Default);
+            query.Subscribe(HandleItem, OnCompleted);
+            Console.ReadLine();
+        }
+
+        private static void HandleItem(string item)
+        {
+            Console.WriteLine($"{item} - {Thread.CurrentThread.ManagedThreadId}");
+        }
+
+        private static void OnCompleted()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Done");
+        }
+    }
+}
