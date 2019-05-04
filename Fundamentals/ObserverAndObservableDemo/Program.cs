b29fd1cc343352c8594b99af722c165c0db3653b
Dileep Balakrishnan <dileepkrishnan@hotmail.com>
2019-05-05 00:28:14 +0530

    Simple Observable and Observer

---
+++ Fundamentals/ObserverAndObservableDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reactive;
+using System.Reactive.Linq;
+
+namespace ObserverAndObservableDemo
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var observable = "Observable And Observer".ToObservable();
+            var observer = Observer.Create<char>(Console.WriteLine);
+            observable.Subscribe(observer);
+            Console.ReadLine();
+        }
+    }
+}
