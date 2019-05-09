e15bd6a18e617dda47c81bc1fe1c7e035069c59b
Dileep Balakrishnan <dileepkrishnan@hotmail.com>
2019-05-09 13:00:02 +0530

    Demonstrate the usage of Schedulers

---
+++ Fundamentals/ConcurrencyManagementDemo/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using System.Threading;
+using System.Windows;
+
+namespace ConcurrencyManagementDemo
+{
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            txtNumbers.Clear();
+            Enumerable.Range(1, 20).Select(x =>
+                {
+                    Thread.Sleep(100);
+                    return x.ToString();
+                }).ToObservable(NewThreadScheduler.Default).ObserveOn(DispatcherScheduler.Current)
+                .Subscribe(x => { txtNumbers.AppendText(x + "\n"); }, () => Console.WriteLine(@"All Done!"));
+        }
+    }
+}
