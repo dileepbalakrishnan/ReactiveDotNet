73960cc945c60e72adfd81d5f1ec7ce02c6314cf
Dileep Balakrishnan <dileepkrishnan@hotmail.com>
2019-05-09 19:09:43 +0530

    Subscription on separate threads

---
+++ Fundamentals/SchedulerUsageDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using System.Threading;
+
+namespace SchedulerUsageDemo
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine($"Application Thread - {Thread.CurrentThread.ManagedThreadId}");
+            "Hello World".ToObservable(Scheduler.Default).Subscribe(
+                s =>
+                {
+                    Thread.Sleep(100);
+                    Console.WriteLine($"Thread - {Thread.CurrentThread.ManagedThreadId}, Value - {s}");
+                },
+                exception =>
+                {
+                    Console.WriteLine($"Thread - {Thread.CurrentThread.ManagedThreadId}");
+                },
+                () =>
+                {
+                    Console.WriteLine($"Thread - {Thread.CurrentThread.ManagedThreadId}, Completed.");
+                }
+            );
+            Console.ReadLine();
+        }
+    }
+}
