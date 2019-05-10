c3ca99aa977e14cc228d8e91202184b5c35bcd98
Dileep Balakrishnan <dileepkrishnan@hotmail.com>
2019-05-10 20:22:18 +0530

    Demonstrate Repeat and Take

---
+++ Fundamentals/RepeatAndTakeDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Threading;
+
+namespace RepeatAndTakeDemo
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // This should print 1 five times with an inetraval of 250ms between two prints
+            Enumerable.Range(1, 1).ToObservable().Repeat().Take(5).Subscribe(x =>
+            {
+                Thread.Sleep(250);
+                Console.WriteLine(x);
+            });
+        }
+    }
+}
