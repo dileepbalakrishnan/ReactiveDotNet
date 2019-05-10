9b3608f724019d8ab7de6746966b857f4067316c
Dileep Balakrishnan <dileepkrishnan@hotmail.com>
2019-05-11 00:09:31 +0530

    Demonstrate the usage of buffer by count and time

---
+++ Fundamentals/BufferUsage/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Threading;
+
+
+namespace BufferUsage
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Enumerable.Range(1, 100).ToObservable().Buffer(10).Subscribe(buffer =>
+            {
+                Console.WriteLine($"Buffer Size is {buffer.Count}");
+                foreach (var item in buffer)
+                {
+                    Console.WriteLine(item);
+                }
+            });
+
+            Enumerable.Range(1, 100).Select( DoSomeWork).
+                ToObservable().Buffer(TimeSpan.FromSeconds(3)).Subscribe(buffer =>
+            {
+                Console.WriteLine($"Buffer Size is {buffer.Count}");
+                foreach (var item in buffer)
+                {
+                    Console.WriteLine(item);
+                }
+            });
+        }
+
+        private static int DoSomeWork(int i)
+        {
+            Thread.Sleep(i*10);
+            return i;
+        }
+    }
+}
