8eff7650b61b5263a7c8f24b90d1058567df9f1d
Dileep Balakrishnan <dileepkrishnan@hotmail.com>
2019-05-10 21:23:41 +0530

    Demonstrate the usage of Scan

---
+++ Fundamentals/ScanUsage/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace ScanUsage
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            new[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}.ToObservable().Scan((x, y) => x + y).Subscribe(Console.WriteLine);
+        }
+    }
+}
